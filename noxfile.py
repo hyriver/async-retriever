19f4ec7f5d66d92ba206b125dbec19816bd700ca
cheginit <cheginit@gmail.com>
2021-06-07 11:42:45 -0500

    ENH: Migrate nox and ward for testing from pytest

---
+++ noxfile.py
@@ -0,0 +1,15 @@
+import nox
+
+@nox.session
+def tests(session):
+    session.install(".[test]")
+    session.run("coverage", "erase")
+    session.run("coverage", "run", "-m", "ward")
+    session.run("pytest")
+    session.run("coverage", "report")
+    session.run("coverage", "html")
+
+
+@nox.session
+def lint(session):
+    session.run("pre-commit", "run", "--all-files", external=True)
