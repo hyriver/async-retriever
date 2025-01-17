18e2e87512451e8389200ff01d8e5e010aa40adb
Taher Chegini <cheginit@gmail.com>
2025-01-17 13:35:53 -0500

    ENH: Add limit_per_host parameter and set its default 5 to avoid hammering services. [skip ci]

--- src/async_retriever/async_retriever.py
+++ src/async_retriever/async_retriever.py
@@ -4,9 +4,8 @@ from __future__ import annotations
 
 import asyncio
 import os
-from typing import TYPE_CHECKING, Any, Literal, Union, overload
+from typing import TYPE_CHECKING, Any, Literal, Union
 
-import cytoolz.curried as tlz
 import ujson as json
 from aiohttp import ClientSession, TCPConnector
 from aiohttp_client_cache import SQLiteBackend
@@ -23,7 +22,7 @@ if TYPE_CHECKING:
 
     from aiohttp.typedefs import StrOrURL
 
-    RESPONSE = Union[
+    Response = Union[
         "list[str]",
         "list[bytes]",
         "list[dict[str, Any]]",
@@ -36,13 +35,12 @@ __all__ = [
     "retrieve_binary",
     "retrieve_json",
     "retrieve_text",
-    "stream_write",
 ]
 
 
 def delete_url_cache(
     url: StrOrURL,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
+    request_method: Literal["get", "post"] = "get",
     cache_name: Path | str | None = None,
     **kwargs: dict[str, Any],
 ) -> None:
@@ -62,8 +60,8 @@ def delete_url_cache(
     """
     loop, new_loop = utils.get_event_loop()
 
-    valid_methods = ["GET", "POST"]
-    if request_method.upper() not in valid_methods:
+    valid_methods = ["get", "post"]
+    if request_method.lower() not in valid_methods:
         raise InputValueError("method", valid_methods)
 
     loop.run_until_complete(
@@ -73,115 +71,23 @@ def delete_url_cache(
         loop.close()
 
 
-async def stream_session(
-    url_kwds: tuple[tuple[Path, StrOrURL, dict[str, Any]], ...],
-    request_method: Literal["get", "GET", "post", "POST"],
-    ssl: SSLContext | bool = True,
-    chunk_size: int | None = None,
-) -> None:
-    """Create an async session for sending requests.
-
-    Parameters
-    ----------
-    url_kwds : list of tuples
-        A list of file paths and URLs or file paths, URLs, and their payloads
-        to be saved as a file.
-    request_method : str
-        The request type; ``GET`` or ``POST``.
-    ssl : bool or SSLContext, optional
-        SSLContext to use for the connection, defaults to ``None``. Set to
-        ``False`` to disable SSL certification verification.
-    chunk_size : int, optional
-        The size of the chunks in bytes to be written to the file, defaults to ``None``,
-        which will iterates over data chunks and write them as received from
-        the server.
-    """
-    async with ClientSession(
-        json_serialize=json.dumps, trust_env=True, connector=TCPConnector(ssl=ssl)
-    ) as session:
-        request_func = getattr(session, request_method.lower())
-        tasks = (
-            utils.stream_session(url, kwds, request_func, filepath, chunk_size)
-            for filepath, url, kwds in url_kwds
-        )
-        await asyncio.gather(*tasks)
-
-
-def stream_write(
-    urls: Sequence[StrOrURL],
-    file_paths: list[str | Path],
-    request_kwds: Sequence[dict[str, Any]] | None = None,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
-    max_workers: int = 8,
-    ssl: SSLContext | bool = True,
-    chunk_size: int | None = None,
-) -> None:
-    r"""Send async requests.
-
-    Parameters
-    ----------
-    urls : list of str
-        List of URLs.
-    file_paths : list of str or pathlib.Path
-        List of file paths to write the response to.
-    request_kwds : list of dict, optional
-        List of requests keywords corresponding to input URLs (1 on 1 mapping),
-        defaults to ``None``. For example, ``[{"params": {...}, "headers": {...}}, ...]``.
-    request_method : str, optional
-        Request type; ``GET`` (``get``) or ``POST`` (``post``). Defaults to ``GET``.
-    max_workers : int, optional
-        Maximum number of async processes, defaults to 8.
-    ssl : bool or SSLContext, optional
-        SSLContext to use for the connection, defaults to None. Set to False to disable
-        SSL certification verification.
-    chunk_size : int, optional
-        The size of the chunks in bytes to be written to the file, defaults to ``None``,
-        which will iterates over data chunks and write them as received from
-        the server.
-
-    Examples
-    --------
-    >>> import async_retriever as ar
-    >>> import tempfile
-    >>> url = "https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_CSV-1.csv"
-    >>> with tempfile.NamedTemporaryFile() as temp:
-    ...     ar.stream_write([url], [temp.name])
-    """
-    inp = BaseRetriever(
-        urls,
-        file_paths=file_paths,
-        request_kwds=request_kwds,
-        request_method=request_method,
-        ssl=ssl,
-    )
-
-    loop, new_loop = utils.get_event_loop()
-
-    session = tlz.partial(
-        stream_session,
-        request_method=inp.request_method,
-        ssl=inp.ssl,
-        chunk_size=chunk_size,
-    )
-
-    chunked_reqs = tlz.partition_all(max_workers, inp.url_kwds)
-
-    _ = [loop.run_until_complete(session(url_kwds=c)) for c in chunked_reqs]
-    if new_loop:
-        loop.close()
-
-
-async def async_session_with_cache(
-    url_kwds: tuple[tuple[int, StrOrURL, dict[str, Any]], ...],
-    read: Literal["text", "json", "binary"],
+async def _session_with_cache(
+    url_kwds: zip[tuple[int, StrOrURL, dict[str, Any]]],
+    read: Literal["read", "text", "json"],
     r_kwds: dict[str, Any],
-    request_method: Literal["get", "GET", "post", "POST"],
+    request_method: Literal["get", "post"],
     cache_name: Path,
     timeout: int = 5,
     expire_after: int = EXPIRE_AFTER,
     ssl: SSLContext | bool = True,
     raise_status: bool = True,
-) -> RESPONSE:
+    limit_per_host: int = 5,
+) -> (
+    list[tuple[int, str | None]]
+    | list[tuple[int, bytes | None]]
+    | list[tuple[int, dict[str, Any] | None]]
+    | list[tuple[int, list[dict[str, Any]] | None]]
+):
     """Create an async session for sending requests.
 
     Parameters
@@ -189,7 +95,7 @@ async def async_session_with_cache(
     url_kwds : list of tuples of urls and payloads
         A list of URLs or URLs with their payloads to be retrieved.
     read : str
-        The method for returning the request; ``binary`` (bytes), ``json``, and ``text``.
+        The method for returning the request; ``read`` (bytes), ``json``, and ``text``.
     r_kwds : dict
         Keywords to pass to the response read function. ``{"content_type": None}`` if read
         is ``json`` else it's empty.
@@ -208,6 +114,8 @@ async def async_session_with_cache(
     raise_status : bool, optional
         Raise an exception if the response status is not 200. If
         ``False`` return ``None``. Defaults to ``True``.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
 
     Returns
     -------
@@ -226,25 +134,31 @@ async def async_session_with_cache(
     async with CachedSession(
         json_serialize=json.dumps,
         cache=cache,
-        connector=TCPConnector(ssl=ssl),
+        connector=TCPConnector(ssl=ssl, limit_per_host=limit_per_host),
         trust_env=True,
     ) as session:
         request_func = getattr(session, request_method.lower())
-        tasks = (
+        tasks = [
             utils.retriever(uid, url, kwds, request_func, read, r_kwds, raise_status)
             for uid, url, kwds in url_kwds
-        )
+        ]
         return await asyncio.gather(*tasks)  # pyright: ignore[reportReturnType]
 
 
-async def async_session_without_cache(
-    url_kwds: tuple[tuple[int, StrOrURL, dict[str, Any]], ...],
-    read: Literal["text", "json", "binary"],
+async def _session_without_cache(
+    url_kwds: zip[tuple[int, StrOrURL, dict[str, Any]]],
+    read: Literal["read", "text", "json"],
     r_kwds: dict[str, Any],
-    request_method: Literal["get", "GET", "post", "POST"],
+    request_method: Literal["get", "post"],
     ssl: SSLContext | bool = True,
     raise_status: bool = True,
-) -> RESPONSE:
+    limit_per_host: int = 5,
+) -> (
+    list[tuple[int, str | None]]
+    | list[tuple[int, bytes | None]]
+    | list[tuple[int, dict[str, Any] | None]]
+    | list[tuple[int, list[dict[str, Any]] | None]]
+):
     """Create an async session for sending requests.
 
     Parameters
@@ -252,7 +166,7 @@ async def async_session_without_cache(
     url_kwds : list of tuples of urls and payloads
         A list of URLs or URLs with their payloads to be retrieved.
     read : str
-        The method for returning the request; ``binary`` (bytes), ``json``, and ``text``.
+        The method for returning the request; ``read`` (bytes), ``json``, and ``text``.
     r_kwds : dict
         Keywords to pass to the response read function. ``{"content_type": None}`` if read
         is ``json`` else it's empty.
@@ -264,6 +178,8 @@ async def async_session_without_cache(
     raise_status : bool, optional
         Raise an exception if the response status is not 200. If
         ``False`` return ``None``. Defaults to ``True``.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
 
     Returns
     -------
@@ -271,77 +187,31 @@ async def async_session_without_cache(
         An async gather function
     """
     async with ClientSession(
-        json_serialize=json.dumps, trust_env=True, connector=TCPConnector(ssl=ssl)
+        json_serialize=json.dumps,
+        trust_env=True,
+        connector=TCPConnector(ssl=ssl, limit_per_host=limit_per_host),
     ) as session:
         request_func = getattr(session, request_method.lower())
-        tasks = (
+        tasks = [
             utils.retriever(uid, url, kwds, request_func, read, r_kwds, raise_status)
             for uid, url, kwds in url_kwds
-        )
+        ]
         return await asyncio.gather(*tasks)  # pyright: ignore[reportReturnType]
-
-
-@overload
-def retrieve(
-    urls: Sequence[StrOrURL],
-    read_method: Literal["text"],
-    request_kwds: Sequence[dict[str, Any]] | None = ...,
-    request_method: Literal["get", "GET", "post", "POST"] = ...,
-    max_workers: int = ...,
-    cache_name: Path | str | None = ...,
-    timeout: int = ...,
-    expire_after: int = ...,
-    ssl: SSLContext | bool = ...,
-    disable: bool = ...,
-    raise_status: bool = ...,
-) -> list[str]: ...
-
-
-@overload
-def retrieve(
-    urls: Sequence[StrOrURL],
-    read_method: Literal["json"],
-    request_kwds: Sequence[dict[str, Any]] | None = ...,
-    request_method: Literal["get", "GET", "post", "POST"] = ...,
-    max_workers: int = ...,
-    cache_name: Path | str | None = ...,
-    timeout: int = ...,
-    expire_after: int = ...,
-    ssl: SSLContext | bool = ...,
-    disable: bool = ...,
-    raise_status: bool = ...,
-) -> list[dict[str, Any]] | list[list[dict[str, Any]]]: ...
-
-
-@overload
-def retrieve(
-    urls: Sequence[StrOrURL],
-    read_method: Literal["binary"],
-    request_kwds: Sequence[dict[str, Any]] | None = ...,
-    request_method: Literal["get", "GET", "post", "POST"] = ...,
-    max_workers: int = ...,
-    cache_name: Path | str | None = ...,
-    timeout: int = ...,
-    expire_after: int = ...,
-    ssl: SSLContext | bool = ...,
-    disable: bool = ...,
-    raise_status: bool = ...,
-) -> list[bytes]: ...
 
 
 def retrieve(
     urls: Sequence[StrOrURL],
     read_method: Literal["text", "json", "binary"],
     request_kwds: Sequence[dict[str, Any]] | None = None,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
-    max_workers: int = 8,
+    request_method: Literal["get", "post"] = "get",
+    limit_per_host: int = 5,
     cache_name: Path | str | None = None,
     timeout: int = 5,
     expire_after: int = EXPIRE_AFTER,
     ssl: SSLContext | bool = True,
     disable: bool = False,
     raise_status: bool = True,
-) -> RESPONSE:
+) -> list[str | bytes | dict[str, Any] | list[dict[str, Any]] | None]:
     r"""Send async requests.
 
     Parameters
@@ -355,8 +225,8 @@ def retrieve(
         defaults to ``None``. For example, ``[{"params": {...}, "headers": {...}}, ...]``.
     request_method : str, optional
         Request type; ``GET`` (``get``) or ``POST`` (``post``). Defaults to ``GET``.
-    max_workers : int, optional
-        Maximum number of async processes, defaults to 8.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
     cache_name : str, optional
         Path to a file for caching the session, defaults to ``./cache/aiohttp_cache.sqlite``.
     timeout : int, optional
@@ -405,50 +275,51 @@ def retrieve(
     if not disable:
         disable = os.getenv("HYRIVER_CACHE_DISABLE", "false").lower() == "true"
 
+    loop, new_loop = utils.get_event_loop()
     if disable:
-        session = tlz.partial(
-            async_session_without_cache,
-            read=inp.read_method,
-            r_kwds=inp.r_kwds,
-            request_method=inp.request_method,
-            ssl=inp.ssl,
-            raise_status=raise_status,
+        results = loop.run_until_complete(
+            _session_without_cache(
+                inp.url_kwds,
+                inp.read_method,
+                inp.r_kwds,
+                inp.request_method,
+                ssl,
+                raise_status,
+                limit_per_host,
+            )
         )
     else:
-        session = tlz.partial(
-            async_session_with_cache,
-            read=inp.read_method,
-            r_kwds=inp.r_kwds,
-            request_method=inp.request_method,
-            cache_name=inp.cache_name,
-            timeout=timeout,
-            expire_after=expire_after,
-            ssl=inp.ssl,
-            raise_status=raise_status,
+        results = loop.run_until_complete(
+            _session_with_cache(
+                inp.url_kwds,
+                inp.read_method,
+                inp.r_kwds,
+                inp.request_method,
+                inp.cache_name,
+                timeout,
+                expire_after,
+                ssl,
+                raise_status,
+                limit_per_host,
+            )
         )
-
-    chunked_reqs = tlz.partition_all(max_workers, inp.url_kwds)
-    loop, new_loop = utils.get_event_loop()
-    results = (loop.run_until_complete(session(url_kwds=c)) for c in chunked_reqs)
-
-    resp = [r for _, r in sorted(tlz.concat(results))]
     if new_loop:
         loop.close()
-    return resp
+    return [r for _, r in sorted(results)]
 
 
 def retrieve_text(
     urls: Sequence[StrOrURL],
     request_kwds: Sequence[dict[str, Any]] | None = None,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
-    max_workers: int = 8,
+    request_method: Literal["get", "post"] = "get",
+    limit_per_host: int = 5,
     cache_name: Path | str | None = None,
     timeout: int = 5,
     expire_after: int = EXPIRE_AFTER,
     ssl: SSLContext | bool = True,
     disable: bool = False,
     raise_status: bool = True,
-) -> list[str]:
+) -> list[str | None]:
     r"""Send async requests and get the response as ``text``.
 
     Parameters
@@ -460,8 +331,8 @@ def retrieve_text(
         defaults to ``None``. For example, ``[{"params": {...}, "headers": {...}}, ...]``.
     request_method : str, optional
         Request type; ``GET`` (``get``) or ``POST`` (``post``). Defaults to ``GET``.
-    max_workers : int, optional
-        Maximum number of async processes, defaults to 8.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
     cache_name : str, optional
         Path to a file for caching the session, defaults to ``./cache/aiohttp_cache.sqlite``.
     timeout : int, optional
@@ -498,12 +369,12 @@ def retrieve_text(
     >>> resp[0].split("\n")[-2].split("\t")[1]
     '01646500'
     """
-    return retrieve(
+    return retrieve(  # pyright: ignore[reportReturnType]
         urls,
         "text",
         request_kwds,
         request_method,
-        max_workers,
+        limit_per_host,
         cache_name,
         timeout,
         expire_after,
@@ -516,15 +387,15 @@ def retrieve_text(
 def retrieve_json(
     urls: Sequence[StrOrURL],
     request_kwds: Sequence[dict[str, Any]] | None = None,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
-    max_workers: int = 8,
+    request_method: Literal["get", "post"] = "get",
+    limit_per_host: int = 5,
     cache_name: Path | str | None = None,
     timeout: int = 5,
     expire_after: int = EXPIRE_AFTER,
     ssl: SSLContext | bool = True,
     disable: bool = False,
     raise_status: bool = True,
-) -> list[dict[str, Any]] | list[list[dict[str, Any]]]:
+) -> list[dict[str, Any] | None] | list[list[dict[str, Any]] | None]:
     r"""Send async requests and get the response as ``json``.
 
     Parameters
@@ -536,8 +407,8 @@ def retrieve_json(
         defaults to ``None``. For example, ``[{"params": {...}, "headers": {...}}, ...]``.
     request_method : str, optional
         Request type; ``GET`` (``get``) or ``POST`` (``post``). Defaults to ``GET``.
-    max_workers : int, optional
-        Maximum number of async processes, defaults to 8.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
     cache_name : str, optional
         Path to a file for caching the session, defaults to ``./cache/aiohttp_cache.sqlite``.
     timeout : int, optional
@@ -575,12 +446,12 @@ def retrieve_json(
     >>> print(r[0]["features"][0]["properties"]["identifier"])
     2675320
     """
-    return retrieve(
+    return retrieve(  # pyright: ignore[reportReturnType]
         urls,
         "json",
         request_kwds,
         request_method,
-        max_workers,
+        limit_per_host,
         cache_name,
         timeout,
         expire_after,
@@ -593,15 +464,15 @@ def retrieve_json(
 def retrieve_binary(
     urls: Sequence[StrOrURL],
     request_kwds: Sequence[dict[str, Any]] | None = None,
-    request_method: Literal["get", "GET", "post", "POST"] = "GET",
-    max_workers: int = 8,
+    request_method: Literal["get", "post"] = "get",
+    limit_per_host: int = 5,
     cache_name: Path | str | None = None,
     timeout: int = 5,
     expire_after: int = EXPIRE_AFTER,
     ssl: SSLContext | bool = True,
     disable: bool = False,
     raise_status: bool = True,
-) -> list[bytes]:
+) -> list[bytes] | list[bytes | None]:
     r"""Send async requests and get the response as ``bytes``.
 
     Parameters
@@ -613,8 +484,8 @@ def retrieve_binary(
         defaults to ``None``. For example, ``[{"params": {...}, "headers": {...}}, ...]``.
     request_method : str, optional
         Request type; ``GET`` (``get``) or ``POST`` (``post``). Defaults to ``GET``.
-    max_workers : int, optional
-        Maximum number of async processes, defaults to 8.
+    limit_per_host : int, optional
+        Maximum number of simultaneous connections per host, defaults to 5.
     cache_name : str, optional
         Path to a file for caching the session, defaults to ``./cache/aiohttp_cache.sqlite``.
     timeout : int, optional
@@ -636,12 +507,12 @@ def retrieve_binary(
     bytes
         List of responses in the order of input URLs.
     """
-    return retrieve(
+    return retrieve(  # pyright: ignore[reportReturnType]
         urls,
         "binary",
         request_kwds,
         request_method,
-        max_workers,
+        limit_per_host,
         cache_name,
         timeout,
         expire_after,
