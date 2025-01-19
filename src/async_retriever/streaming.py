af670dcd6463868574766eda8d34df8556cbaac0
Taher Chegini <cheginit@gmail.com>
2025-01-19 14:55:08 -0500

    ENH: Add a new function for generating a unique filename from a web query. [skip ci]

--- src/async_retriever/streaming.py
+++ src/async_retriever/streaming.py
@@ -3,37 +3,33 @@
 from __future__ import annotations
 
 import asyncio
-from collections.abc import Sequence
+import hashlib
+import json
 from pathlib import Path
-from ssl import SSLContext
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Any
 
 import aiofiles
 from aiohttp import ClientSession, ClientTimeout, TCPConnector
+from multidict import MultiDict
+from yarl import URL
 
-from async_retriever._utils import get_event_loop
-from async_retriever.exceptions import DownloadError
+from async_retriever import _utils as utils
+from async_retriever.exceptions import InputTypeError, ServiceError
 
 if TYPE_CHECKING:
-    from aiohttp.typedefs import StrOrURL
+    from collections.abc import Sequence
 
-__all__ = ["stream_write"]
+__all__ = ["generate_filename", "stream_write"]
 CHUNK_SIZE = 1024 * 1024  # Default chunk size of 1 MB
+MAX_HOSTS = 4  # Maximum connections to a single host (rate-limited service)
 TIMEOUT = 10 * 60  # Timeout for requests in seconds
 
 
-async def _stream_file(
-    session: ClientSession,
-    request_method: Literal["get", "post"],
-    url: StrOrURL,
-    filepath: Path,
-    chunk_size: int,
-) -> None:
-    """Stream the response to a file."""
-    async with session.request(request_method, url) as response:
+async def _stream_file(session: ClientSession, url: str, filepath: Path, chunk_size: int) -> None:
+    """Stream the response to a file, skipping if already downloaded."""
+    async with session.get(url) as response:
         if response.status != 200:
-            filepath.unlink(missing_ok=True)
-            raise DownloadError(await response.text(), str(response.url))
+            raise ServiceError(await response.text(), str(response.url))
         remote_size = int(response.headers.get("Content-Length", -1))
         if filepath.exists() and filepath.stat().st_size == remote_size:
             return
@@ -43,93 +39,92 @@ async def _stream_file(
                 await file.write(chunk)
 
 
-async def _stream_session(
-    url_file_mappings: zip[tuple[StrOrURL, Path]],
-    request_method: Literal["get", "post"],
-    ssl: bool | SSLContext,
-    chunk_size: int,
-    limit_per_host: int,
-) -> None:
-    """Create an async session to download files."""
-    if isinstance(ssl, bool):
-        verify_ssl = ssl
-        ssl_context = None
-    elif isinstance(ssl, SSLContext):
-        verify_ssl = True
-        ssl_context = ssl
-    else:
-        raise TypeError("`ssl` must be a boolean or SSLContext object.")
-
+async def _stream_session(urls: Sequence[str], files: Sequence[Path], chunk_size: int) -> None:
+    """Download files concurrently."""
     async with ClientSession(
-        connector=TCPConnector(
-            verify_ssl=verify_ssl,
-            ssl_context=ssl_context,
-            limit_per_host=limit_per_host,
-        ),
+        connector=TCPConnector(limit_per_host=MAX_HOSTS),
         timeout=ClientTimeout(TIMEOUT),
     ) as session:
         tasks = [
-            _stream_file(session, request_method, url, filepath, chunk_size)
-            for url, filepath in url_file_mappings
+            asyncio.create_task(_stream_file(session, url, filepath, chunk_size))
+            for url, filepath in zip(urls, files)
         ]
         await asyncio.gather(*tasks)
 
 
 def stream_write(
-    urls: Sequence[StrOrURL],
-    file_paths: Sequence[Path] | Sequence[str],
-    request_method: Literal["get", "post"] = "get",
-    ssl: bool | SSLContext = True,
-    chunk_size: int = CHUNK_SIZE,
-    limit_per_host: int = 5,
+    urls: Sequence[str], file_paths: Sequence[Path], chunk_size: int = CHUNK_SIZE
 ) -> None:
     """Download multiple files concurrently by streaming their content to disk.
 
     Parameters
     ----------
-    urls : Sequence[str]
-        List of URLs to download.
-    file_paths : Sequence[Path]
-        List of file paths to save the downloaded content.
-    request_method : {"get", "post"}, optional
-        HTTP method to use (i.e., ``get`` or ``post``), by default ``get``.
-    ssl : bool or ssl.SSLContext, optional
-        Whether to verify SSL certificates, by default True. Also,
-        an SSLContext object can be passed to customize
+    urls : list of str
+        URLs to download.
+    file_paths : list of pathlib.Path
+        Paths to save the downloaded files.
     chunk_size : int, optional
-        Size of each chunk in bytes, by default 1 MB.
-    limit_per_host : int, optional
-        Maximum simultaneous connections per host, by default 5.
-
-    Examples
-    --------
-    >>> import tempfile
-    >>> url = "https://freetestdata.com/wp-content/uploads/2021/09/Free_Test_Data_500KB_CSV-1.csv"
-    >>> with tempfile.NamedTemporaryFile(dir=".") as temp:
-    ...     stream_write([url], [temp.name])
+        Size of the chunks to download, by default 1 MB.
     """
-    if not isinstance(urls, Sequence) or not isinstance(file_paths, Sequence):
-        raise TypeError("`urls` and `file_paths` must be sequences of URLs and file paths.")
-    if len(urls) != len(file_paths):
-        raise TypeError("`urls` and `file_paths` must be sequences of same length.")
-
     file_paths = [Path(filepath) for filepath in file_paths]
-    parent_dirs = {filepath.parent for filepath in file_paths}
-    for parent_dir in parent_dirs:
+    if len(urls) != len(file_paths):
+        raise InputTypeError("urls/files_paths", "lists of the same size")
+
+    for parent_dir in {f.parent for f in file_paths}:
         parent_dir.mkdir(parents=True, exist_ok=True)
 
-    loop, is_new_loop = get_event_loop()
+    utils.run_in_event_loop(_stream_session(urls, file_paths, chunk_size))
 
-    try:
-        loop.run_until_complete(
-            _stream_session(
-                zip(urls, file_paths),
-                request_method,
-                ssl,
-                chunk_size,
-                limit_per_host,
-            )
-        )
-    finally:
-        if is_new_loop:
-            loop.close()
+
+def generate_filename(
+    url: str,
+    params: dict[str, Any] | MultiDict[str, Any] | None = None,
+    data: dict[str, Any] | str | None = None,
+    prefix: str | None = None,
+    file_extension: str = "",
+) -> str:
+    """Generate a unique filename using SHA-256 from a query.
+
+    Parameters
+    ----------
+    url : str
+        The URL for the request.
+    params : dict, multidict.MultiDict, optional
+        Query parameters for the request, default is ``None``.
+    data : dict, str, optional
+        Data or JSON to include in the hash, default is ``None``.
+    prefix : str, optional
+        A custom prefix to attach to the filename, default is ``None``.
+    file_extension : str, optional
+        The file extension to append to the filename, default is ``""``.
+
+    Returns
+    -------
+    str
+        A unique filename with the SHA-256 hash, optional prefix, and
+        the file extension.
+    """
+    url_obj = URL(url)
+
+    if params is not None and not isinstance(params, (dict, MultiDict)):
+        raise InputTypeError("params", "dict or multidict.MultiDict.")
+
+    if data is not None and not isinstance(data, (dict, str)):
+        raise InputTypeError("data", "dict or str.")
+
+    if params:
+        params_obj = MultiDict(params)
+        url_obj = url_obj.with_query(params_obj)
+    params_str = str(url_obj.query or "")
+
+    if isinstance(data, dict):
+        data_str = json.dumps(data, sort_keys=True, separators=(",", ":"))
+    else:
+        data_str = str(data or "")
+
+    prefix_part = prefix or ""
+    hash_input = f"{url_obj.human_repr()}{params_str}{data_str}"
+    hash_digest = hashlib.sha256(hash_input.encode("utf-8")).hexdigest()
+    file_extension = file_extension.lstrip(".")
+    file_extension = f".{file_extension}" if file_extension else ""
+    return f"{prefix_part}{hash_digest}{file_extension}"
